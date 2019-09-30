5269f052f12f5680eff87f79a6df7ae2dc139f78
liaozhongxun <869664233@qq.com>
2019-09-30 17:34:44 +0800

    updata

--- ja.js
+++ ja.js
@@ -1,4 +1,14 @@
-var a =10;
-for(var c =0;c<=5;c++){
- console.log(c)
-}
+
+
+var twoSum = function (nums, target) {
+    let ind = [];
+    nums.map((it1, in1) => {
+        nums.map((it2, in2) => {
+            if (in1 != in2 && it1 + it2 == target) {
+                ind = [in1, in2]
+            }
+        })
+    })
+    return ind.sort((a, b) => a - b);
+};
+console.log(twoSum([2, 7, 11, 15], 22));
