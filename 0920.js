fca94a9ac313f18a2406efdd3f6c7d225eaf9a7a
liaozhongxun <869664233@qq.com>
2019-11-21 17:25:45 +0800

    updata

--- 0920.js
+++
@@ -1,9 +0,0 @@
-let i;
-for (i = 0; i < 3; i++) {
-  var log = (j) => {
-    console.log(j);
-  }
-  setTimeout(function(i){
-    log(i)
-  }, 100);
-}
---
+++ 1121.js
@@ -0,0 +1,15 @@
+// let i;
+// for (i = 0; i < 3; i++) {
+//   var log = (j) => {
+//     console.log(j);
+//   }
+//   setTimeout(function (i) {
+//     log(i)
+//   }, 100);
+// }
+console.time('test1');
+var arr = 1000000000000000;
+ setTimeout(()=>{
+   
+ },10000)
+ console.timeEnd('test1');
