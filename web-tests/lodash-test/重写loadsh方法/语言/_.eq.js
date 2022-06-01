aaaa936ac6e235c335f5514e95d69e1ceeb77a10
liaozhongxun <869664233@qq.com>
2022-06-01 17:48:23 +0800

    save

---
+++ web-tests/lodash-test/重写loadsh方法/语言/_.eq.js
@@ -0,0 +1,25 @@
+
+/**
+ * lodash 方法
+ *
+ *
+ */
+
+let _ = require("lodash");
+var object = { 'a': 1 };
+var other = { 'a': 1 };
+var b = object;
+
+console.log(_.eq(object, object))
+console.log(_.eq(object, b))
+
+
+/**
+ * 实现
+ *
+ */
+
+ let m_xxx = () => {
+
+};
+console.log(m_xxx())
