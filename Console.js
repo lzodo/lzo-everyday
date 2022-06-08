9a360097244fc5ec6fd41d0566452e3a70fd0db6
liaozhongxun <869664233@qq.com>
2022-06-08 18:02:06 +0800

    save

--- Console.js
+++ Console.js
@@ -1,16 +1,21 @@
-//运行时间
-const startTime = performance.now();
-let sum = 0;
-for(let i = 0;i < 100000;i++) {
-    sum += i;
-}
-const diffTime = performance.now() - startTime;
-console.log(`Execution time: ${ diffTime }`);
-// =================
+// //运行时间
+// const startTime = performance.now();
+// let sum = 0;
+// for(let i = 0;i < 100000;i++) {
+//     sum += i;
+// }
+// const diffTime = performance.now() - startTime;
+// console.log(`Execution time: ${ diffTime }`);
+// // =================
 
-console.time('sum');
-let sum = 0;
-for(let i = 0;i < 100000;i++) {
-    sum += i;
-}
-console.timeEnd('sum');
+// console.time('sum');
+// let sum = 0;
+// for(let i = 0;i < 100000;i++) {
+//     sum += i;
+// }
+// console.timeEnd('sum');
+
+console.log(String.fromCharCode(97))
+console.log(escape("了"))
+console.log(JSON.parse('{"char":"\u4e00"}'));
+console.log("一".charCodeAt(0))
