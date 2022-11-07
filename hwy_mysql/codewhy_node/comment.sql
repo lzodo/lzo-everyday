d70e3f478bf52d016c59c269e9f18662b0e41efc
lenovo-kali <869664233@qq.com>
2022-11-07 08:25:04 +0800

    2022-11-07 08:24提交上传

--- hwy_mysql/codewhy_node/comment.sql
+++ hwy_mysql/codewhy_node/comment.sql
@@ -16,3 +16,17 @@ ALTER TABLE `comment` ADD `updateAt` TIMESTAMP;
 
 ALTER TABLE `comment` MODIFY createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE `comment` MODIFY updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+SELECT
+	c.id id,c.content content,c.dynamic_id dynamicId,c.comment_id commentId,c.createAt createTime,
+	JSON_OBJECT('id',u.id,'name',u.name,'createTime',u.createAt)
+	FROM comment c LEFT JOIN users u ON c.user_id = u.id
+	WHERE c.id = 31;
+
+# 多对多
+# 点赞 （ 一个用户可以给多条动态点赞，一个动态可以被多个用户点赞 ）
+#		dyname_id + 点赞用户的 user_id 建立关系表
+# 标签  ( 一个动态可以有多个标签，一个标签可以属于多个动态 ) 
+
+
+
