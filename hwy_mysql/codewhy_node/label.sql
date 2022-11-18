5d77c27f24238a7938a67fdc8c203dc3a3931d95
liaozhongxun <869664233@qq.com>
2022-11-18 17:05:50 +0800

    2022-11-18 17:05提交上传

--- hwy_mysql/codewhy_node/label.sql
+++ hwy_mysql/codewhy_node/label.sql
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS label (
 );
 
 -- label 的增加查询
-
+INSERT INTO label (name) VALUES ("SESSIO");
 -- 创建动态和标签的关系表 dynamic_label
 
-CREATE TABLE IF NOT EXISTS dynamic_lable (
+CREATE TABLE IF NOT EXISTS dynamic_label (
 	dynamic_id INT NOT NULL,
 	label_id INT NOT NULL,
 	createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -22,8 +22,17 @@ CREATE TABLE IF NOT EXISTS dynamic_lable (
 	PRIMARY KEY(dynamic_id, label_id), # 复合或联合主键
 	FOREIGN KEY (dynamic_id) REFERENCES dynamic(id) ON DELETE CASCADE ON UPDATE CASCADE,   # 动态删除，这个关系表的记录同时删除，
 	FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE CASCADE ON UPDATE CASCADE  # 标签删除，这个关系表的记录同时删除
-)
+);
 
+SELECT 
+		dy.id id,content,
+		JSON_OBJECT('id',us.id,'name',us.name) as users, 		
+		JSON_ARRAYAGG(JSON_OBJECT('id',lb.id,'name',lb.name)) as labels
+FROM dynamic dy 
+LEFT JOIN users us ON dy.user_id = us.id 
+LEFT JOIN dynamic_label dyla ON dy.id = dyla.dynamic_id
+LEFT JOIN label lb ON dyla.label_id = lb.id 
+WHERE dy.id = 10;
 
 -- 给动态添加标签
 -- 得到用户传入的标签名 
