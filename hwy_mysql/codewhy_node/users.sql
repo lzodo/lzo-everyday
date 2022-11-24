50e2567c0d84f8a2f06d32a301d6b321b4ef26b9
liaozhongxun <869664233@qq.com>
2022-11-24 17:10:19 +0800

    2022-11-24 17:09提交上传

--- hwy_mysql/codewhy_node/users.sql
+++ hwy_mysql/codewhy_node/users.sql
@@ -1,5 +1,5 @@
 DELETE FROM users WHERE id >= 57 AND id <= 62;
 
-ALTER TABLE users ADD sex VARCHAR(10);
+ALTER TABLE users ADD open_id VARCHAR(100);
 ALTER TABLE users ADD headurl VARCHAR(100);
 ALTER TABLE users ADD display_name VARCHAR(100);
