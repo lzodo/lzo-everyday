bc10afe5bc7ee389cab3e3dd3f2e0f8f3f4d6546
lenovo-kali <869664233@qq.com>
2022-11-25 08:23:45 +0800

    2022-11-25 08:23提交上传

--- hwy_mysql/codewhy_node/users.sql
+++ hwy_mysql/codewhy_node/users.sql
@@ -3,3 +3,4 @@ DELETE FROM users WHERE id >= 57 AND id <= 62;
 ALTER TABLE users ADD open_id VARCHAR(100);
 ALTER TABLE users ADD headurl VARCHAR(100);
 ALTER TABLE users ADD display_name VARCHAR(100);
+
---
+++ hwy_mysql/mysql2_test/11.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `t_user` ( 
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID', 
+	`id_no` varchar(18) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '身份编号', 
+	`username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '用户名', 
+	`age` int(11) DEFAULT NULL COMMENT '年龄', 
+	`create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间', 
+	PRIMARY KEY (`id`), 
+	KEY `union_idx` (`id_no`,`username`,`age`), 
+	KEY `create_time_idx` (`create_time`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+select * from t_user where username = 'Tom2' and age = 12;
+select id_no, username, age from t_user where username = 'Tom2';
+
+select * from user where name='一灯'
+select * from t_user where username = 'Tom2' and age = 12;
+select id_no, username, age from t_user where username = 'Tom2'; 
