3f7f822e4a7f2bb524f05dadb4bbd17581987501
lenovo-kali <869664233@qq.com>
2022-12-18 11:15:01 +0800

    2022-12-18 11:14提交上传

--- react/coderwhy/day91_React全家桶实战_12/code/05_learn_redux/src/store/constants.js
+++
@@ -1,7 +0,0 @@
-const ADD_NUMBER = "add_number"
-const CHANGE_NAME = "change_name"
-
-module.exports = {
-  ADD_NUMBER,
-  CHANGE_NAME
-}
--- react/coderwhy/day91_React全家桶实战_12/code/05_learn_redux/src/util.js
+++
@@ -1,5 +0,0 @@
-const store = require("./store")
-
-const { changeNameAction } = require("./store/actionCreators")
-
-store.dispatch(changeNameAction("curry"))
