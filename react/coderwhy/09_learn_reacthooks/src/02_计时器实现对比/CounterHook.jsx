a5e1c09b4403b4a772b10ad7af5c7ddcfeb1af74
lenovo-kali <869664233@qq.com>
2023-01-16 08:42:13 +0800

    2023-01-16 08:41提交上传

--- react/coderwhy/09_learn_reacthooks/src/02_计时器实现对比/CounterHook.jsx
+++ react/coderwhy/09_learn_reacthooks/src/02_计时器实现对比/CounterHook.jsx
@@ -8,20 +8,19 @@ import { memo, useState } from "react";
 // }
 
 function CounterHook(props) {
-  const [counter, setCounter] = useState(0)
-  const [name] = useState("why")
-  console.log(name)
+    const [counter, setCounter] = useState(0);
+    const [name] = useState("why");
+    console.log(name);
 
-  // const message = useFoo()
+    // const message = useFoo()
 
-  return (
-    <div>
-      <h2>当前计数: {counter}</h2>
-      <button onClick={e => setCounter(counter+1)}>+1</button>
-      <button onClick={e => setCounter(counter-1)}>-1</button>
-    </div>
-  )
+    return (
+        <div>
+            <h2>当前计数: {counter}</h2>
+            <button onClick={(e) => setCounter(counter + 1)}>+1</button>
+            <button onClick={(e) => setCounter(counter - 1)}>-1</button>
+        </div>
+    );
 }
 
-export default memo(CounterHook)
-
+export default memo(CounterHook);
