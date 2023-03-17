973fda267ef7325abc878074bb600b7354c9de3c
lenovo-kali <869664233@qq.com>
2023-03-18 00:56:40 +0800

    2023-03-18 00:56提交上传

--- hwy_mysql/codewhy_node/roles.sql
+++ hwy_mysql/codewhy_node/roles.sql
@@ -11,7 +11,9 @@ CREATE TABLE IF NOT EXISTS roles (
 
 # 角色名称 roleName、权限字符 roleKey、角色顺序 roleSort、是否启用 enable、权限菜单 menuIds、备注 remark
 
-ALTER TABLE `roles` CHANGE `roleName` `role_name` VARCHAR(50); 
+ALTER TABLE `roles` CHANGE `role_name` `name` VARCHAR(50); 
 ALTER TABLE `roles` CHANGE `roleKey` `role_key` VARCHAR(50); 
 ALTER TABLE `roles` CHANGE `roleSort` `role_sort` INT; 
 ALTER TABLE `roles` CHANGE `isEnable` `is_enable` TINYINT(1); 
+
+INSERT INTO `codewhy_node`.`roles` (`role_name`, `role_key`, `role_sort`, `is_enable`, `createAt`, `remark`, `updateAt`) VALUES ('普通用户', 'base', 1, 1, '2023-02-28 20:19:55', '用户普通用户角色', '2023-02-28 20:20:02');
--- hwy_mysql/codewhy_node/users.sql
+++ hwy_mysql/codewhy_node/users.sql
@@ -4,3 +4,4 @@ ALTER TABLE users ADD open_id VARCHAR(100);
 ALTER TABLE users ADD headurl VARCHAR(100);
 ALTER TABLE users ADD display_name VARCHAR(100);
 
+INSERT INTO `codewhy_node`.`users` (`name`, `passwd`, `createAt`, `updataAt`, `sex`, `headurl`, `display_name`, `open_id`) VALUES ( 'lzoxxun3', '250cf8b51c773f3f8dc8b4be867a9a02', '2023-02-27 13:56:40', '2023-02-27 13:56:40', NULL, '/images/head_g.png', NULL, NULL);
